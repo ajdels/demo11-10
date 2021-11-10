a8d30e6f8c9307f06b885e8e5ba342765eab83c5
ajdels <89940365+ajdels@users.noreply.github.com>
2021-11-10 15:04:17 -0500

    commit 2

---
+++ src/main/java/com/example/demo1110/DataHandler.java
@@ -0,0 +1,22 @@
+package com.example.demo1110;
+
+import java.net.http.HttpClient;
+import java.util.ArrayList;
+
+public class DataHandler(String webLocation){
+    dataGrabber =
+    private String webLocation;
+
+    class universityDataType{
+        String alpha_two_code;
+        ArrayList<String>web_pages;
+        String name;
+        String country;
+        ArrayList<String> domains;
+
+        @Override
+        public String toString(){
+            return name;
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.demo1110 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
 
     opens com.example.demo1110 to javafx.fxml;
     exports com.example.demo1110;
