1d831c08e65e9398d95d69d71be0e2336dec3a84
ajdels <89940365+ajdels@users.noreply.github.com>
2021-11-10 14:41:13 -0500

    Initial commit

---
+++ src/main/java/com/example/demo1110/UniversityController.java
@@ -0,0 +1,14 @@
+package com.example.demo1110;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class UniversityController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
